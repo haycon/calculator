029d24d28907e81b53a249ad6e22b9614649b482
Håkon Johansen <haycon@protonmail.com>
2020-09-12 16:19:58 +0200

    Worked on the operation function

--- script.js
+++ script.js
@@ -3,28 +3,43 @@ let value = 0;
 let stringValue = "";
 let array = [];
 let buttonPressed = "";
+let calc = document.getElementById("calculation")
 
-
+//Shows numbers when pressing the buttons
 function display(){
 
     let text = document.getElementById(event.target.id)
-    let calc = document.getElementById("calculation")
     
-    stringValue += text.innerHTML
-    value = Number(stringValue)
+    stringValue += parseInt(text.innerHTML)
+    value = parseInt(stringValue)
     calc.innerHTML += text.innerHTML
-    console.log(value)
 }
 
+//Pushes numbers into an array
 function add(arr){
     buttonPressed = "add"
     array.push(value)
-    console.log(array)
-    return arr.reduce((total, current) => total + current, 0)
+
+    value = 0;
+    stringValue = "";
+
+    calc.innerHTML += " + "
+}
+
+function add2 (a,b) {
+	let sum = 0;
+	sum += a + b;
+	return sum;
 }
 
 function subtract(arr){
     return arr.reduce((total, current) => total - current, 0)
+}
+
+function subtract2 (a,b) {
+	let sum = 0;
+	sum += a - b;
+	return sum;
 }
 
 function multiply(arr){
@@ -39,40 +54,12 @@ function divide(arr){
     : 0;
 }
 
-
-function add (a,b) {
-	let sum = 0;
-	sum += a + b;
-	return sum;
-}
-
-function subtract (a,b) {
-	let sum = 0;
-	sum += a - b;
-	return sum;
-}
-
-
-function sum (array) {
-	return array.reduce((total,current) => total+current,0);
-}
-
-
-function multiply (array) {
-	return array.reduce((a,b) => a*b);
-}
-
-//Does this function need to have inputs?
-function operate(operator,num1,num2){
+//Runs the operation that is pressed
+function operate(){
     
-    //if(buttonPressed == "add"){
-    //    add()
-    //}
-
-    console.log("hello")
+    if(buttonPressed == "add"){
+        array.push(value)
+        let sum = array.reduce((total, current) => total + current, 0)
+        calc.innerHTML = sum
+    }
 }
-
-
-let a = [2,1]
-
-console.log(add(a))
