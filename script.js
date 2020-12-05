31010b37ec408450a4fe78a78537be567b56ff7d
haycon <46656475+haycon@users.noreply.github.com>
2020-12-05 17:52:39 +0100

    Script done

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-let firstArray = [];
-let firstNumber = 0;
-let secondNumber = 0;
-let firstSave = 0;
-let operation = '';
-let var1 = document.getElementById('firstNum');
-let var2 = document.getElementById('firstSave');
-let var5 = document.getElementById('num5');
-let calc = document.getElementById('calculation');
 let numbers = document.querySelectorAll('.numbers');
+let calc = document.getElementById('calculation');
+let var1 = document.getElementById('num1');
+let var2 = document.getElementById('num2');
+let firstArray = [];
+let num1 = 0;
+let num2 = 0;
+let operation = '';
 
+//Show tmp var and stored var
 window.addEventListener('click', () => {
-  var1.textContent = `First number ${firstNumber}`;
-  var2.innerHTML = `Second number ${secondNumber}`;
-  var5.innerHTML = `First save num ${firstSave}`;
+  var1.innerHTML = `First number ${num1}`;
+  var2.innerHTML = `Second number ${num2}`;
 });
 
+//Onclick for each number
 numbers.forEach((e) => e.addEventListener('click', display));
 
 //Adds number into variable when clicked
@@ -22,7 +21,7 @@ function display(e) {
   let buttonValue = parseFloat(e.path[0].value);
 
   firstArray.push(buttonValue);
-  firstNumber = parseFloat(firstArray.join(''));
+  num1 = parseFloat(firstArray.join(''));
 
   calc.innerHTML += buttonValue;
 }
@@ -31,91 +30,75 @@ function display(e) {
 clear.addEventListener('click', () => {
   operation = '';
   firstArray = [];
-  firstNumber = 0;
-  secondNumber = 0;
-  firstSave = 0;
+  num1 = 0;
+  num2 = 0;
   secondSave = 0;
   calc.innerHTML = '';
 });
 
+//Add
 addition.addEventListener('click', () => {
-  if (firstSave == 0) {
-    firstSave = firstNumber;
+  if (num2 == 0) {
+    num2 = num1;
   } else {
-    firstSave = eq(firstNumber, firstSave, operation);
+    num2 = eq(num1, num2, operation);
   }
   operation = 'addition';
   calc.innerHTML += '+';
   firstArray = [];
 });
 
+//Subtraction
 subtraction.addEventListener('click', () => {
-  if (firstSave == 0) {
-    firstSave = firstNumber;
+  if (num2 == 0) {
+    num2 = num1;
   } else {
-    firstSave = eq(firstNumber, firstSave, operation);
+    num2 = eq(num1, num2, operation);
   }
   operation = 'subtraction';
   calc.innerHTML += '-';
   firstArray = [];
 });
 
+//Multiplication
 multiplication.addEventListener('click', () => {
-  if (firstSave == 0) {
-    firstSave = firstNumber;
+  if (num2 == 0) {
+    num2 = num1;
   } else {
-    firstSave = eq(firstNumber, firstSave, operation);
+    num2 = eq(num1, num2, operation);
   }
   operation = 'multiplication';
   calc.innerHTML += 'x';
   firstArray = [];
 });
 
+//Division
 division.addEventListener('click', () => {
-  if (firstSave == 0) {
-    firstSave = firstNumber;
+  if (num2 == 0) {
+    num2 = num1;
   } else {
-    firstSave = eq(firstNumber, firstSave, operation);
+    num2 = eq(num1, num2, operation);
   }
   operation = 'division';
   calc.innerHTML += '/';
   firstArray = [];
 });
 
-let eq = (firstNumber, firstSave, operation) => {
+equals.addEventListener('click', () => {
+  calc.innerHTML = eq(num1, num2, operation);
+});
+
+let eq = (num1, num2, operation) => {
   if (operation == 'addition') {
-    return firstNumber + firstSave;
+    return num1 + num2;
   }
   if (operation == 'subtraction') {
-    return firstSave - firstNumber;
+    return num2 - num1;
   }
   if (operation == 'multiplication') {
-    return firstSave * firstNumber;
+    return num2 * num1;
   }
   if (operation == 'division') {
-    return firstSave / firstNumber;
+    return num2 / num1;
   }
 };
-
-equals.addEventListener('click', () => {
-  if (operation == 'addition') {
-    secondNumber = firstNumber;
-    calc.innerHTML = firstSave + firstNumber;
-    firstNumber = 0;
-  }
-
-  if (operation == 'subtraction') {
-    calc.innerHTML = firstSave - firstNumber;
-    firstNumber = 0;
-  }
-
-  if (operation == 'division') {
-    calc.innerHTML = firstSave / firstNumber;
-    firstNumber = 0;
-  }
-
-  if (operation == 'multiplication') {
-    calc.innerHTML = firstSave * firstNumber;
-    firstNumber = 0;
-  }
-});
